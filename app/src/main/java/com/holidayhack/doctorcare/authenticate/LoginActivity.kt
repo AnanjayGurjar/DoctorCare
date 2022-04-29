3e38cfdf72e06c4da10c66f8661e6c512c3dfc91
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-04-29 20:02:50 +0530

    login_signup ui

--- app/src/main/java/com/holidayhack/doctorcare/authenticate/LoginActivity.kt
+++ app/src/main/java/com/holidayhack/doctorcare/authenticate/LoginActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.holidayhack.doctorcare.R
 
+
+//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
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

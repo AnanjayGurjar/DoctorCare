9baefcd7258b5116f8de94feca9cb856275a526e
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-04-29 18:52:57 +0530

    setup

---
+++ app/src/main/java/com/holidayhack/doctorcare/authenticate/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.holidayhack.doctorcare.authenticate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.holidayhack.doctorcare.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

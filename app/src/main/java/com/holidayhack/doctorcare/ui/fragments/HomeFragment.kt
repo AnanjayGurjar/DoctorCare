625d5b94d28ee57db5d70559e0072e1067f3daea
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-04-30 17:36:58 +0530

    last one

--- app/src/main/java/com/holidayhack/doctorcare/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/holidayhack/doctorcare/ui/fragments/HomeFragment.kt
@@ -6,17 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.holidayhack.doctorcare.R
+import com.holidayhack.doctorcare.databinding.FragmentCreatePatientBinding
 import com.holidayhack.doctorcare.databinding.FragmentHomeBinding
 
 
 class FirstFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
 
 }
